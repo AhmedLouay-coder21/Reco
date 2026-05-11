8d9980070c90a5f9bba2711c488a8da59367d6db
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2026-05-11 14:58:09 +0300

    feat: add mock login calls in login page and add api login fetch toggle for testing

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,6 +1,18 @@
 let diagonalLineDir = 0;
 let textPos = 0;
 let titlePos = 0;
+const loginMock = async (credentials) => {
+    await new Promise(res => setTimeout(res, 1000)); // time out 1 second to simulate network lag
+    if (credentials.email === "admin@test.com" && credentials.password === "12345678") {
+        return { token: "abc-123-xyz", user: { name: "Ahmad" } };
+    }
+    throw new Error("Invalid email or password");
+};
+
+// this should be toggled to false after the backend logic is done
+const isDev = false;
+const login = (creds) => isDev ? loginMock(creds) : fetch('/api/v1/auth/login', { /* fetch config */ });
+
 export function Login() 
 {
     const main = document.querySelector("main");
@@ -24,6 +36,8 @@ export function Login()
         "absolute inset-0 animate-aurora bg-aurora-gradient bg-[length:300%_200%] z-0";
 
     const loginBox1 = GenerateLoginForm();
+    const loginForm = loginBox1.querySelector("#loginForm");
+    loginForm.addEventListener("submit", handleLoginSubmit);
 
     const signUpButton = loginBox1.querySelector("#signupBtn");
     signUpButton.addEventListener("click", () => {
@@ -113,10 +127,13 @@ function BackToLogin()
     textTitle.style.transform = `translateX(${titlePos}vw)`;
     diagonalLine.style.transform = `rotate(${diagonalLineDir}deg)`;
 
-    // Replace signup form with login form
+    // regenerate signup form
     const newLoginForm = GenerateLoginForm();
-
     logInBox.innerHTML = newLoginForm.innerHTML;
+
+    // reattach listeners so they are not lost
+    const loginForm = logInBox.querySelector("#loginForm");
+    loginForm.addEventListener("submit", handleLoginSubmit);
 
     // Reattach signup button event
     const signupBtn = logInBox.querySelector("#signupBtn");
@@ -131,29 +148,29 @@ function GenerateLoginForm()
     loginBox1.id = "loginBox1";
     loginBox1.innerHTML = `<div class = "ml-10 mt-10 text-4xl text-(--color-text-dif)"> Welcome back </div>
         <div class = "ml-12 mt-5 text-xl text-(--color-text-dif)"> Login to your account </div>
-        <form>
+        <form id = "loginForm">
             <div id = "entryFields" class=" m-10 flex flex-col w-[150%]">
-            <div class="mb-6">
-                <input type="email" id="email" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Email address" required />
-            </div>
-            <div class="mb-6">
-                <input type="password" id="password" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Password" required />
-            </div>
-            <div class="flex flex-col gap-6 max-w-sm mx-auto relative z-10">
-            <button class="group relative p-4 rounded-2xl backdrop-blur-xl border-2 border-(--color-border) bg-gradient-to-br from-(--color-line-dim)/35 via-(--color-secondary)/55 to-(--color-main) shadow-2xl hover:shadow-[0_0_24px_var(--color-glow)] hover:scale-[1.02] hover:-translate-y-1 active:scale-95 transition-all duration-500 ease-out cursor-pointer hover:border-(--color-line-bright)/60 overflow-hidden">
-                <div class="absolute inset-0 bg-gradient-to-r from-transparent via-(--color-line-bright) to-transparent opacity-40 -translate-x-full group-hover:translate-x-full transition-transform duration-1000 ease-out"></div>
-                <div class="absolute inset-0 rounded-2xl bg-gradient-to-r from-(--color-secondary)/10 via-(--color-line-bright)/25 to-(--color-secondary)/10 opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
-
-                <div class="relative z-10 flex items-center gap-4 w-50">
-                    <div class="flex-1 text-left">
-                        <p class="text-center text-[#fff] font-bold text-lg group-hover:text-(--color-line-bright) transition-colors duration-300 drop-shadow-sm">
-                        LOG IN
-                        </p>
-                    </div>
+                <div class="mb-6">
+                    <input type="email" id="email" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Email address" required />
                 </div>
-            </button>
+                <div class="mb-6">
+                    <input type="password" id="password" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Password" required />
+                </div>
+                <div class="flex flex-col gap-6 max-w-sm mx-auto relative z-10">
+                <button type="submit" id="loginSubmitBtn" class="group relative p-4 rounded-2xl backdrop-blur-xl border-2 border-(--color-border) bg-gradient-to-br from-(--color-line-dim)/35 via-(--color-secondary)/55 to-(--color-main) shadow-2xl hover:shadow-[0_0_24px_var(--color-glow)] hover:scale-[1.02] hover:-translate-y-1 active:scale-95 transition-all duration-500 ease-out cursor-pointer hover:border-(--color-line-bright)/60 overflow-hidden">
+                    <div class="absolute inset-0 bg-gradient-to-r from-transparent via-(--color-line-bright) to-transparent opacity-40 -translate-x-full group-hover:translate-x-full transition-transform duration-1000 ease-out"></div>
+                    <div class="absolute inset-0 rounded-2xl bg-gradient-to-r from-(--color-secondary)/10 via-(--color-line-bright)/25 to-(--color-secondary)/10 opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
+
+                    <div class="relative z-10 flex items-center gap-4 w-50">
+                        <div class="flex-1 text-left">
+                            <p id = "loginBtnText" class="text-center text-[#fff] font-bold text-lg group-hover:text-(--color-line-bright) transition-colors duration-300 drop-shadow-sm">
+                            LOG IN
+                            </p>
+                        </div>
+                    </div>
+                </button>
             </div>
-            <button id="signupBtn" class="text-center text-(--color-main) text-(--color-secondary) hover:text-(--color-text-dif) transition-colors duration-300 cursor-pointer mt-2">
+            <button type="button" id="signupBtn" class="text-center text-(--color-main) text-(--color-secondary) hover:text-(--color-text-dif) transition-colors duration-300 cursor-pointer mt-2">
                 create new account
             </button>
         </form>
@@ -168,33 +185,33 @@ function GenerateSignUpForm()
         <div class = "ml-12 mt-5 text-xl text-(--color-text-dif)"> Join us now </div>
         <form id="signupForm" m-20>
             <div id = "entryFields" class=" m-10 flex flex-col w-[150%]">
-            <div class="mb-6">
-                <input type="text" id="username" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Username" required />
-            </div>
-            <div class="mb-6">
-                <input type="email" id="email" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Email address" required />
-            </div>
-            <div class="mb-6">
-                <input type="password" id="password" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Password" required />
-            </div>
-            <div class="mb-6">
-                <input type="password" id="passwordCheck" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Re-enter password" required />
-            </div>
-            <div class="flex flex-col gap-6 max-w-sm mx-auto relative z-10">
-            <button class="group relative p-4 rounded-2xl backdrop-blur-xl border-2 border-(--color-border) bg-gradient-to-br from-(--color-line-dim)/35 via-(--color-secondary)/55 to-(--color-main) shadow-2xl hover:shadow-[0_0_24px_var(--color-glow)] hover:scale-[1.02] hover:-translate-y-1 active:scale-95 transition-all duration-500 ease-out cursor-pointer hover:border-(--color-line-bright)/60 overflow-hidden">
-                <div class="absolute inset-0 bg-gradient-to-r from-transparent via-(--color-line-bright) to-transparent opacity-40 -translate-x-full group-hover:translate-x-full transition-transform duration-1000 ease-out"></div>
-                <div class="absolute inset-0 rounded-2xl bg-gradient-to-r from-(--color-secondary)/10 via-(--color-line-bright)/25 to-(--color-secondary)/10 opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
-                
-                <div class="relative z-10 flex items-center gap-4 w-50">
-                    <div class="flex-1 text-left">
-                        <p class="text-center text-[#fff] font-bold text-lg group-hover:text-(--color-line-bright) transition-colors duration-300 drop-shadow-sm">
-                        CREATE ACCOUNT
-                        </p>
-                    </div>
+                <div class="mb-6">
+                    <input type="text" id="username" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Username" required />
                 </div>
-            </button>
+                <div class="mb-6">
+                    <input type="email" id="email" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Email address" required />
+                </div>
+                <div class="mb-6">
+                    <input type="password" id="password" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Password" required />
+                </div>
+                <div class="mb-6">
+                    <input type="password" id="passwordCheck" class="bg-neutral-secondary-medium border border-(--color-secondary) border-default-medium rounded-lg h-15 text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Re-enter password" required />
+                </div>
+                <div class="flex flex-col gap-6 max-w-sm mx-auto relative z-10">
+                <button type="submit" id = "signupSubmitBtn" class="group relative p-4 rounded-2xl backdrop-blur-xl border-2 border-(--color-border) bg-gradient-to-br from-(--color-line-dim)/35 via-(--color-secondary)/55 to-(--color-main) shadow-2xl hover:shadow-[0_0_24px_var(--color-glow)] hover:scale-[1.02] hover:-translate-y-1 active:scale-95 transition-all duration-500 ease-out cursor-pointer hover:border-(--color-line-bright)/60 overflow-hidden">
+                    <div class="absolute inset-0 bg-gradient-to-r from-transparent via-(--color-line-bright) to-transparent opacity-40 -translate-x-full group-hover:translate-x-full transition-transform duration-1000 ease-out"></div>
+                    <div class="absolute inset-0 rounded-2xl bg-gradient-to-r from-(--color-secondary)/10 via-(--color-line-bright)/25 to-(--color-secondary)/10 opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
+
+                    <div class="relative z-10 flex items-center gap-4 w-50">
+                        <div class="flex-1 text-left">
+                            <p class="text-center text-[#fff] font-bold text-lg group-hover:text-(--color-line-bright) transition-colors duration-300 drop-shadow-sm">
+                            CREATE ACCOUNT
+                            </p>
+                        </div>
+                    </div>
+                </button>
             </div>
-            <button type="submit" id="logInBtn" class="text-center text-(--color-main) text-(--color-secondary) hover:text-(--color-text-dif) transition-colors duration-300 cursor-pointer mt-2">
+            <button type="button" id="logInBtn" class="text-center text-(--color-main) text-(--color-secondary) hover:text-(--color-text-dif) transition-colors duration-300 cursor-pointer mt-2">
                 already have an account? login
             </button>
         </form>`;
@@ -247,4 +264,34 @@ function validatePassword(password, passwordCheck) {
         valid: true,
         message: ""
     };
+}
+async function handleLoginSubmit(e)
+{
+    e.preventDefault();
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    const btnText = document.querySelector("#loginBtnText");
+    const submitBtn = document.querySelector("#loginSubmitBtn");
+
+    try
+    {
+        btnText.innerText = "VERIFYING...";
+        submitBtn.disabled = true;
+   
+        const response = await login({ email: email, password: password });
+        localStorage.setItem("auth_token", response.token);
+        
+        alert(`Welcome back, ${response.user.name}!`);
+    }
+
+    catch (error)
+    {
+        alert("Login Failed: " + error.message);
+    }
+    
+    finally
+    {
+        btnText.innerText = "LOG IN";
+        submitBtn.disabled = false;
+    }
 }
